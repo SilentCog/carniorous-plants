e58b51941743a16f83bd9a6414ab655e986ff19b
Mimi Flynn <mimiflynn@gmail.com>
2018-05-06 20:07:35 -0400

    coords test

--- cp-backend/app/config/routes.js
+++ cp-backend/app/config/routes.js
@@ -7,5 +7,21 @@ router.get('/', function(req, res, next) {
   res.send('Beware the carnivorous plants!');
 });
 
+router.get('/mimi', function(req, res, next) {
+  var position = {
+    lat: 40.6799909,
+    lon: -73.9680101
+  };
+  res.send(position);
+});
+
+router.get('/sandy', function(req, res, next) {
+  var position = {
+    lat: 40.6799909,
+    lon: -73.9680101
+  };
+  res.send(position);
+});
+
 export default router;
 
